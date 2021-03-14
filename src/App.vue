35c980080b7a2c07fa3bdbdb24d7af64c119cab8
mnjoyanx <tigranmnjoyan@gmail.com>
2021-03-14 22:30:35 +0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-modal :title="title" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VModal from './components/V-Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VModal
+  },
+  data() {
+    return {
+      title: 'some title'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/V-Modal.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="modal--wrapper">
+        <div class="modal--content">
+            <div class="modal--header">
+                <span class="modal--title"> {{ title }}} </span>
+                <span class="modal--close"></span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-modal',
+        props: {
+            title: {
+                type: String,
+                default: 'V-Modal'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .modal--wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        transition: opacity .3s ease;
+        right: 0;
+        z-index: 999;
+        background-color: rgba(0, 0, 0, .48);
+    }
+
+    .modal--content {
+        position: realative;
+        max-width: 600px;
+        padding: 20px 18px;
+        background-color: #fff;
+          
+    }
+</style>
